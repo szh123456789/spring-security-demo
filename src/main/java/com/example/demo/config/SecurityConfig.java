594ee1761302d7b256ccccfe34ba057e9e02ad77
szh <1696646098@qq.com>
2022-09-01 18:26:40 +0800

    修改security配置

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -19,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //执行登录逻辑的url
                 .loginProcessingUrl("/login")
                 //成功跳转地址
-//                .successForwardUrl("/toMain")
 //                .successForwardUrl("/success")
                 //设置跳转成功handler 适合前后端分离
 //                .successHandler(new AuthenticationSuccessHandler() {
@@ -41,22 +43,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                })
                 .failureUrl("http://www.smallming.com")
                 .usernameParameter("myusername")
-                .passwordParameter("mypassword");
-
-        //设置url授权
-        //多个条件取交集
-        http.authorizeRequests()
+                .passwordParameter("mypassword")
+                .and()
+                //设置url授权
+                //多个条件取交集
+                .authorizeRequests()
                 //匹配 / 控制器 permitAll()不需要被认证即可访问
                 .antMatchers("/").permitAll()
                 // anyRequest()所有请求 authenticated()必须被认证
-                .anyRequest().authenticated();
-
-        // 关闭跨域
-        http.csrf().disable();
+                .anyRequest().authenticated()
+                .and()
+                // 关闭跨域
+                .csrf().disable();
     }
 
     @Bean
     public PasswordEncoder getPwdEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
     }
 }
